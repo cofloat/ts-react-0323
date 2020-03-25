36b59cf0f565b7f51eaf22588ee3defdf3c4f017
Kazuyuki Hosoya <kaho@kahoMBP.local>
2020-03-25 18:14:51 +0900

    initial

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+#firebase/config.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4FKfv191qwK3jPpQPYeCg9vkvkuRLlhE",
+    authDomain: "cofloat-react.firebaseapp.com",
+    databaseURL: "https://cofloat-react.firebaseio.com",
+    projectId: "cofloat-react",
+    storageBucket: "cofloat-react.appspot.com",
+    messagingSenderId: "790442252248",
+    appId: "1:790442252248:web:451f3c6714c614bf1e44ad",
+    measurementId: "G-PTF4RBF3EP"
+};
